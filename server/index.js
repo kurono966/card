1154ad4a4e1886c4ccac9d925bd6eb39f97a0c1c
Hirokuni Kato <krt432krt432@gmail.com>
2025-08-09 00:38:57 +0900

    Fix block phase logic

--- server/index.js
+++ server/index.js
@@ -243,7 +243,6 @@ io.on('connection', (socket) => {
             console.log(`[Server] Player ${socket.id} played ${card.name}, requesting target for 2 damage.`);
           }
           // 他の効果があればここに追加
-        }
 
       } else {
         console.log(`[Server] Player ${socket.id} tried to play card ${card.value}, but not enough mana. Cost: ${card.manaCost}, Current: ${players[socket.id].currentMana}`);
